bb80e096f765bbee2a92a13dd8c1381530d7ef90
rubik16x16 <drt_mike@hotmail.com>
2020-01-09 15:45:21 -0400

    Eliminar pruebas unitarias vacias

--- src/app/directives/can.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CanDirective } from './can.directive';
-
-describe('CanDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CanDirective();
-    expect(directive).toBeTruthy();
-  });
-});
